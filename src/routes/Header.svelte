bf29c54556c9ac5e4a10187cc8853c60cac4b391
julianbaffy <142730946+julianbaffy@users.noreply.github.com>
2025-08-20 23:56:15 +0200

    Comment Link to Pygamepage from header

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -23,9 +23,9 @@
 			<li aria-current={page.url.pathname === '/websites' ? 'page' : undefined}>
 				<a href="/websites">Websites</a>
 			</li>
-			<li aria-current={page.url.pathname === '/pygames' ? 'page' : undefined}>
+			<!--<li aria-current={page.url.pathname === '/pygames' ? 'page' : undefined}>
 				<a href="/pygames">Pygames</a>
-			</li>
+			</li>-->
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
