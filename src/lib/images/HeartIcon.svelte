6df6e41cd78665f85249a324186433f39a4b345f
julianbaffy <142730946+julianbaffy@users.noreply.github.com>
2026-09-13 02:17:16 +0200

    Update Designs for Icons

--- src/lib/images/HeartIcon.svelte
+++ src/lib/images/HeartIcon.svelte
@@ -9,7 +9,7 @@
 		/>
 	</svg>
 {:else}
-	<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" xmlns="http://www.w3.org/2000/svg">
+	<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" xmlns="http://www.w3.org/2000/svg">
 		<path
 			stroke-linecap="round"
 			stroke-linejoin="round"
